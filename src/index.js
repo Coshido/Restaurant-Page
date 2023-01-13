3d598d1004eec0964f78d35a86718623794fa036
Coshido <coshido@gmail.com>
2023-01-13 16:45:19 +0100

    add navbar

--- src/index.js
+++ src/index.js
@@ -11,14 +11,32 @@ function createHeader() {
   headerLogo.innerHTML = "Baistrocchi Caffè";
   header.appendChild(headerLogo);
 
+  let navBar = createNav();
+  header.appendChild(navBar);
   divContent.appendChild(header);
 }
 
 function createNav() {
   let divNav = document.createElement("div");
+  divNav.classList.add("nav-bar");
 
   let homeButton = document.createElement("button");
   homeButton.classList.add("nav-button");
+  homeButton.innerHTML = "Home";
+
+  let menuButton = document.createElement("button");
+  menuButton.classList.add("nav-button");
+  menuButton.innerHTML = "Menù";
+
+  let contactButton = document.createElement("button");
+  contactButton.classList.add("nav-button");
+  contactButton.innerHTML = "Contacts";
+
+  divNav.appendChild(homeButton);
+  divNav.appendChild(menuButton);
+  divNav.appendChild(contactButton);
+
+  return divNav;
 }
 
 function createMain() {
