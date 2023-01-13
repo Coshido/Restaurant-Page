db1a14c06577c72e2cbd17b62f4201b0eb97a65e
Coshido <coshido@gmail.com>
2023-01-13 14:59:07 +0100

    add footer element and fonts to project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./style.css";
+import createFooter from "./footer.js";
 
-console.log("hello lelelel");
+const divContent = document.querySelector("#content");
+
+function createHeader() {
+  let header = document.createElement("header");
+  divContent.appendChild(header);
+}
+
+function createMain() {
+  let main = document.createElement("div");
+  divContent.appendChild(main);
+}
+
+function render() {
+  createHeader();
+  createMain();
+  divContent.appendChild(createFooter());
+}
+
+render();
